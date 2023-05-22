dba84d45894b52c5f26c738fc125f0b784e6ee68
CindySong6 <cindysong622@gmail.com>
2023-05-21 23:57:38 -0400

    blogs

--- website/views.py
+++ website/views.py
@@ -1,26 +1,58 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, flash, redirect, request, url_for
 from flask_login import login_required, current_user
 from .create_db import Blog, User
 from . import db
 
 # don't know how to query with foreign key with sqlalchemy
 
-dummy_blog = [{'title':'Ramen Ipsum', 'published_date': 'May 10, 2023','content':'Soy sauce salt miso butter roasted pork slices flavoured oil yuzu seasoned egg spicy bean paste, kamaboko Kumamoto Tokushima Nissin instant cup ramen rice scallions bean sprouts, chilli toasted sesame seeds lard sesame oil nori bamboo slices soy milk.', 'user_id':1},{'title' :'Bacon Ipsum', 'content' : 'Beef chicken pork bacon chuck shortloin sirloin shank aute turkey, hamhock eiusmod labore laboris anim et spareribs minim, stripsteak exercitation sausage deserunt filetmignon ut kevin magna. Do officia qui hamburger eiusmod porkbelly leberkas aliquip nisi, swine chuck nostrud venison dolore sausage ea velit nulla, beef fugiat tenderloin exercitation adipisicing quis sirloin.', 'user_id':2}]
-
 views = Blueprint('views', __name__)
 #Home Page Not Signed In
 @views.route('/')
 def home():
-    return render_template("home.html", user=current_user, blogs_data=dummy_blog)
+    blogs = Blog.query.all()
+    print(blogs)
+    return render_template("home.html", user=current_user, blogs_data=blogs)
 
 @views.route('/dashboard')
 def dashboard():
-    blogs_data = dummy_blog
+    blogs_data = Blog.query.filter_by(user_id = current_user.id)
+    print(blogs_data)
     return render_template("dashboard.html", user=current_user, blogs_data=blogs_data)
 
-@views.route('/add_blog')
+@views.route('/blog/<id>')
+def blog(id):
+    blog = Blog.query.get(id)
+    return render_template('blog.html', user=current_user, blog=blog)
+
+@views.route('/add_blog', methods=['GET', 'POST'])
 def add_blog():
-    return render_template('add_blog.html')
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        new_blog = Blog(title=title,content=content, user_id=current_user.id)
+        db.session.add(new_blog)
+        db.session.commit()
+        flash("Posted!", category='success')
+        return render_template("dashboard.html", user=current_user)
+
+    return render_template('add_blog.html', user=current_user)
+
+@views.route('/blog/delete/<int:id>')
+def delete_blog(id):
+    blog_to_delete = Blog.query.get_or_404(id)
+    try:
+        db.session.delete(blog_to_delete)
+        db.session.commit()
+        flash("Blog was deleted!", category="success")
+        return render_template("dashboard.html", user=current_user)
+    except:
+        flash("it did not get deleted!", category="error")
+    return render_template("dashboard.html", user=current_user)
+
+
+
+
 
 
 
